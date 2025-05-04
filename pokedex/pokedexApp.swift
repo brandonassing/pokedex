02c8e2632a53cb01cd2e376ad4c0eb8e1ee5ea50
Brandon Assing <assing.brandon@gmail.com>
2025-05-04 19:10:42 -0400

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Brandon Assing on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pokedexTests/pokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexTests.swift
+//  pokedexTests
+//
+//  Created by Brandon Assing on 2025-05-04.
+//
+
+import Testing
+@testable import pokedex
+
+struct pokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
